155826a0d9d89ca3f782b4fa7c46240099af8349
JLCodeSource <JLCodeSource@gmail.com>
2022-07-21 15:45:11 +0100

    Last failed attempt working on files vs dicts

--- log-aggregator.py
+++ log-aggregator.py
@@ -1,14 +1,59 @@
 import os
+from pickle import FALSE
 import zipfile
 import fileinput
 import sys
-#import csv
+import csv
 from pathlib import Path
 from shutil import move
+
+from typing import Optional
+from pydantic import BaseModel
+from odmantic import AIOEngine, Model
+from motor.motor_asyncio import AsyncIOMotorClient
+from datetime import datetime
+import re
 
 # Vars
 sourcedir = "./source"
 outdir = "./out"
+connection = "mongodb://root:example@localhost:27017/?authMechanism=DEFAULT"
+database = "logs"
+
+
+class Log(Model):
+    node: str  # Indexed(str)
+    datetime: datetime
+    message: str  # Indexed(str, pymongo.DESCENDING)
+
+    class Config:
+        collection = "logs"
+        anystr_strip_whitespace = True
+        """ indexes = [
+            [
+                ("node", pymongo.TEXT),
+                ("message", pymongo.TEXT),
+            ]
+        ] """
+
+
+class JavaLog(Log):
+    severity: str  # Indexed(str)
+    jvm: str
+    source: str  # Indexed(str)
+    type: str  # Indexed(str)
+
+    class Settings:
+        collection = "javalogs"
+        """ indexes = [
+            [
+                ("node", pymongo.TEXT),
+                ("message", pymongo.TEXT),
+                ("severity", pymongo.TEXT),
+                ("source", pymongo.TEXT),
+                ("type", pymongo.TEXT),
+            ]
+        ] """
 
 
 def getNode(file: str) -> str:
@@ -50,6 +95,16 @@ def extractLog(file: str, target: str, extension: str):
     os.rmdir(tmplogsout)
 
 
+def multiToSingleLine(logfile, target):
+    data = open(os.path.join(target, logfile)).read().split("\n")
+    for i, line in enumerate(data):
+        if not line.startswith(("INFO", "WARN", "ERROR")):
+            data[i-1] = data[i-1]+line
+            data.pop(i)
+
+    print(data)
+
+
 def enrichLog(logfile, target, node):
     prepend = node + '\t| '
 
@@ -59,20 +114,60 @@ def enrichLog(logfile, target, node):
             prepend=prepend, line=line))
 
 
+def convertLogtoCSV(logfile, target):
+    header = ["node", "severity", "jvm",
+              "datetime", "source", "type", "message"]
+    with open(os.path.join(target, logfile), "r") as file:
+        reader = csv.DictReader(file, delimiter="|", fieldnames=header)
+        return list(reader)
+
+
+async def saveLogs(engine, logs):
+    await engine.save_all(engine, logs)
+
+
 def main():
     # Main app
+
+    client = AsyncIOMotorClient(connection)
+    engine = AIOEngine(motor_client=client, database=database)
+
+    LogList = []
     for file in os.listdir(sourcedir):
         node = getNode(file)
         logtype = getLogType(file)
         logsout = getLogOutputDir(node, logtype)
-        extension = ".log"
+        extension = "service.log"
         createLogsOutputDir(logsout)
 
         extractLog(file, logsout, extension)
 
-    # Work on log files in logsout
-    for logfile in os.listdir(logsout):
-        enrichLog(logfile, logsout, node)
+        # Work on log files in logsout
+        for logfile in os.listdir(logsout):
+            multiToSingleLine(logfile, logsout)
+            enrichLog(logfile, logsout, node)
+            reader = convertLogtoCSV(logfile, logsout)
+
+            for dict in reader:
+                for k, v in dict.items():
+                    if dict[k]:
+                        dict[k] = v.strip()
+
+                timestamp = datetime.strptime(
+                    dict["datetime"], "%Y/%m/%d %H:%M:%S")
+                log = JavaLog(
+                    node=dict["node"],
+                    severity=dict["severity"],
+                    jvm=dict["jvm"],
+                    datetime=timestamp,
+                    source=dict["source"],
+                    type=dict["type"],
+                    message=dict["message"]
+                )
+                LogList.append(log)
+
+            print(LogList)
+            saveLogs(engine, LogList)
 
 
 main()
