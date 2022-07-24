7f5cabaa43915b600ad1b7fe6c99d857b4021bea
JLCodeSource <JLCodeSource@gmail.com>
2022-07-24 13:33:10 +0100

    Added logging to convert

--- convert.py
+++ convert.py
@@ -9,52 +9,59 @@ from datetime import datetime
 from pydantic import ValidationError
 from db import saveLogs
 from model import JavaLog
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def lineStartMatch(match, string):
     # Returns true if the beginning of the string matches match
-    return bool(re.match(match, string))
+    matches = bool(re.match(match, string))
+    logger.debug(f"Matches: {matches} from {match} with '{string}'")
+    return matches
 
 
 def yield_matches(full_log: list[str]):
     # Yield matches creates a list of logs and yields the list on match
-    log = []
+    logs = []
     for line in full_log.split("\n"):
         if lineStartMatch("INFO|WARN|ERROR", line):  # if line matches start
-            if len(log) > 0:  # if there's already a log
-                yield "; ".join(log)  # yield the log
-                log = []  # and set the log back to nothing
+            if len(logs) > 0:  # if there's already a log
+                yield "; ".join(logs)  # yield the log
+                logs = []  # and set the log back to nothing
         # Handle corrupted lines
         lineLen = len(line.split("|"))
-        # if not lineLen == 1 and not lineLen == 6:
-        #   print(lineLen)
-        #   print(line.split("|"))
+        if not lineLen == 1 and not lineLen == 6:
+            logger.warn(f"Found corrupt log entry with {lineLen} sections")
+            logEntry = line.split("|")
+            logger.warn(f"logEntry: {logEntry}")
         if lineLen > 6:
             line = line.split("|")
             line[5] = line[5] + " ***CORRUPTED_NEXT_LOG_FOLLOWS*** "
             for i in range(6, lineLen):
                 line[5] = line[5] + line[i]
                 line.pop(i)
-            #print("New line: \n {line}".format(line=line))
+            logger.warn(f"New line: \n {line}")
         tmpLine = ""
         if type(line) == list:
             for s in line:
                 tmpLine = tmpLine + "," + s
             line = tmpLine[1:]
-            # print(type(line))
-            # print(line)
-        log.append(line.strip())  # add current line to log (list)
+        logs.append(line.strip())  # add current line to log (list)
+        logger.debug(f"Appended: {line} to list")
 
 
 def multiToSingleLine(logfile, target):
     # multiToSingleLine converts multiline to single line logs
     data = open(os.path.join(target, logfile)).read()
-
+    logger.info(f"Opened {logfile} for reading")
     logs = list(yield_matches(data))
 
     with open(os.path.join(target, logfile), "w") as file:
-        for log in logs:
-            file.write("{log}\n".format(log=log))
+        for line in logs:
+            file.write(f"{line}\n")
+            logger.debug(f"Wrote: {line} to {file}")
+        logger.info(f"Wrote converted logs to {logfile}")
 
 
 def convertLogtoCSV(logfile, target):
@@ -63,6 +70,7 @@ def convertLogtoCSV(logfile, target):
               "datetime", "source", "type", "message"]
     with open(os.path.join(target, logfile), "r") as file:
         reader = csv.DictReader(file, delimiter="|", fieldnames=header)
+        logger.info(f"Opened {logfile} as csv.dictReader")
         return list(reader)
 
 
@@ -79,7 +87,7 @@ async def convert(logfile, logsout, node):
                     try:
                         dict[k] = v.strip()
                     except AttributeError as err:
-                        print("Attribute error: {0}".format(err))
+                        logger.warn(f"AttributeError: {err}")
 
             dict["node"] = node
 
@@ -97,8 +105,7 @@ async def convert(logfile, logsout, node):
                 )
                 LogList.append(log)
             except ValidationError as err:
-                print("Validation error: {err}".format(
-                    err=err))
+                logger.warn(f"ValidationError: {err}")
 
     await saveLogs(LogList)
     LogList = []
