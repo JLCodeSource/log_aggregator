dd47ec2739b639c9aaa4221bc2a9a05141164222
JLCodeSource <JLCodeSource@gmail.com>
2022-07-29 17:05:06 +0100

    Class-ifying main to help with testing

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ Functions: main, init, extractLog
 Variables: sourcedir
 """
 
+from pyparsing import empty
 from aggregator.config import get_settings
 from aggregator.convert import convert
 from aggregator.db import client, init, save_logs
@@ -16,12 +17,26 @@ from aggregator.extract import extract_log
 from aggregator.logs import configure_logging
 import logging
 
-logger = logging.getLogger("main")
+logger = logging.getLogger(__name__)
 
 
+class Aggregator:
+    def __init__(self, func):
+        self._func = func
+
+    def __call__(self):
+        return self._func()
+
+
+@Aggregator
 async def main():
 
-    settings = get_settings()
+    try:
+        settings = get_settings()
+        assert (settings is not empty), "Failed to get settings"
+    except AssertionError as err:
+        logger.fatal(f"AssertionError: {err}")
+        exit()
     logger.info("Loading config settings from the environment...")
     logger.debug(f"Environment: {settings.environment}")
     logger.debug(f"Testing: {settings.testing}")
@@ -34,7 +49,13 @@ async def main():
     await init()
 
     # Extact logs from source directory
-    log_files = await extract_log(settings.sourcedir)
+    try:
+        log_files = await extract_log(settings.sourcedir)
+        if log_files is None or log_files is empty:
+            raise Exception(
+                f"Failed to get log_files from {settings.sourcedir}")
+    except Exception as err:
+        logger.error(f"{err}")
 
     for file in log_files:
         log_list = await convert(file)
