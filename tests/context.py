6b005ed2759067599a1ebfaddee62c891d8264b4
JLCodeSource <JLCodeSource@gmail.com>
2022-08-03 15:48:21 +0100

    Removed unnecessary files

--- tests/context.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-sys.path.insert(
-    0,
-    os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                 '..')))
-from aggregator import config  # noqa
-
-settings = config.get_settings()
