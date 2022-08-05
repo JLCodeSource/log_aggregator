d4c937c4121476f56757c80f190cbb4bc6794c94
JLCodeSource <JLCodeSource@gmail.com>
2022-08-05 12:56:11 +0100

    Removed some race conditions in db tests

--- tests/test_db.py
+++ tests/test_db.py
@@ -20,6 +20,14 @@ class MockBeanie:
         raise ServerSelectionTimeoutError
 
 
+class MockWriteResult:
+    # MockWriteResult is used for testing the database
+
+    @staticmethod
+    def acknowledged():
+        return False
+
+
 @pytest.mark.asyncio
 @pytest.mark.unit
 async def test_init(motor_client_gen, logger, add_one):
@@ -112,7 +120,6 @@ async def test_save_logs(motor_client_gen, logger):
     # And an initialized  database
     try:
         await aggregator.db.init(database, client)
-        logger.set_level = logging.DEBUG
 
         # And 2 logs
         log = JavaLog(
@@ -132,36 +139,86 @@ async def test_save_logs(motor_client_gen, logger):
         result = await aggregator.db.save_logs(logs)
 
         # Then the logger logs it
-        assert logger.record_tuples[3][0] == module_name
-        assert logger.record_tuples[3][1] == logging.INFO
-        assert logger.record_tuples[3][2].startswith(
-            "Started insert coroutine for 2 into db:"
-        )
-        assert logger.record_tuples[4][0] == module_name
-        assert logger.record_tuples[4][1] == logging.INFO
-        assert logger.record_tuples[4][2].startswith(
-            "Inserted 2 into db:"
-        )
-        assert logger.record_tuples[5][0] == module_name
-        assert logger.record_tuples[5][1] == logging.DEBUG
-        assert logger.record_tuples[5][2].startswith(
-            f"Inserted {log}"
-        )
-        assert logger.record_tuples[6][0] == module_name
-        assert logger.record_tuples[6][1] == logging.DEBUG
-        assert logger.record_tuples[6][2].startswith(
-            f"Inserted {log}"
-        )
-        assert logger.record_tuples[7][0] == module_name
-        assert logger.record_tuples[7][1] == logging.INFO
-        assert logger.record_tuples[7][2].startswith(
-            "Ending insert coroutine for 2 into db:"
-        )
+        # Get lists of types
+        modules = []
+        levels = []
+        messages = []
+
+        for recorded_log in logger.record_tuples:
+            modules.append(recorded_log[0])
+            levels.append(recorded_log[1])
+            messages.append(recorded_log[2])
+
+        count_logs = 0
+        for message in messages:
+            if message == f"Inserted {log}":
+                count_logs = count_logs + 1
+
+        count_debugs = 0
+        for level in levels:
+            if level == logging.DEBUG:
+                count_debugs = count_debugs + 1
+
+        count_infos = 0
+        for level in levels:
+            if level == logging.INFO:
+                count_infos = count_infos + 1
+
+        # The logger logs modules
+        assert all(module == module_name for module in modules)
+
+        # And logger logs logs
+        assert count_logs == 2
+
+        # And logger logs levels
+        assert count_debugs == 2
+        assert count_infos == 6
+
+        # And logger includes expected values
+        assert (s.startswith("Inserted 2 into db:") for s in messages)
+        assert (s.startswith("Started insert coroutine for 2 into db:")
+                for s in messages)
+        assert (s.startswith("Ending insert coroutine for 2 into db:")
+                for s in messages)
 
         # And it returns a list of the ids
         assert len(result.inserted_ids) == 2
         assert isinstance(result.inserted_ids[0], ObjectId)
         assert isinstance(result.inserted_ids[1], ObjectId)
+    finally:
+        # Set manual teardown
+        await client.drop_database(database)
+
+
+@ pytest.mark.asyncio
+@ pytest.mark.unit
+async def test_save_logs_fails(motor_client_gen, logger, monkeypatch):
+    # Given a mock init_beanie_server_timeout to target the test database
+    def mock_init_beanie_server_timeout(*args, **kwargs):
+        return MockBeanie.init_beanie_server_timeout()
+
+    monkeypatch.setattr(beanie, "init_beanie",
+                        mock_init_beanie_server_timeout)
+    # And a motor_client generator
+    motor_client = await motor_client_gen
+    # And a motor_client
+    client = motor_client[0][0]
+    # And a database
+    database = motor_client[0][1]
+
+    # When it tries to initialize the database
+    # Then it raises a ServerSelectionTimeoutError
+    try:
+        with pytest.raises(ServerSelectionTimeoutError):
+            await aggregator.db.init(database, client)
+
+        # And the logger logs the error
+        assert logger.record_tuples[1] == (
+            module_name, logging.FATAL,
+            "ServerSelectionTimeoutError: Server was unreachable "
+            "within the timeout"
+        )
+
     finally:
         # Set manual teardown
         await client.drop_database(database)
