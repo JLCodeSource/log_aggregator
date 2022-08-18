9e7d89dd0e5684c80a610bb673bb17929130abdb
JLCodeSource <JLCodeSource@gmail.com>
2022-08-18 23:14:34 +0100

    Adding additional tests for Queues

--- tests/test_main.py
+++ tests/test_main.py
@@ -27,25 +27,66 @@ class TestQueues:
     def test_create_queue_publish_structure(self) -> None:
         # Given a queue function
         # When it tries to add to the queue
-        queue = asyncio.Queue()
+        queue: asyncio.Queue[int] = asyncio.Queue()
         value: int = 1
         asyncio.run(publish(queue, value))
-        
+
         # Then something is added to the queue
         assert asyncio.run(queue.get()) == 1
 
-    
     @pytest.mark.unit
     @pytest.mark.asyncio
     def test_create_queue_consume_structure(self) -> None:
-        # Given a queue function
-        # When it tries to add to the queue
+        # Given a queue
         queue = asyncio.Queue()
+        # When a value is added to the queue
         value: int = 1
         asyncio.run(publish(queue, value))
-        
-        # Then something is added to the queue
+
+        # Then it can read the queue
         assert asyncio.run(consume(queue)) == 1
 
+    @pytest.mark.unit
+    @pytest.mark.asyncio
+    def test_multiple_queues(self) -> None:
+        # Given a queue
+        zip_file_queue = asyncio.Queue()
+        # And another queue
+        log_file_queue = asyncio.Queue()
+
+        # When a value is added to one queue
+        asyncio.run(publish(zip_file_queue, 1))
+        # And a different value is added to the other queue
+        asyncio.run(publish(log_file_queue, 2))
+
+        # Then the value added to the second queue is in the second queue
+        assert asyncio.run(consume(log_file_queue)) == 2
+        # And the value added to the first queue is in the first queue
+        assert asyncio.run(consume(zip_file_queue)) == 1
 
 
+    @pytest.mark.unit
+    @pytest.mark.asyncio
+    def test_adding_files_to_queue(self) -> None:
+        # Given a queue
+        zip_file_queue: asyncio.Queue[str] = asyncio.Queue()
+
+        # And a mock dirlist of files
+        dirlist: list[str] = []
+        for i in range(10):
+            dirlist.append(f"file{i}.zip")
+
+        # When a list of zip files is added to it
+        for file in dirlist:
+            asyncio.run(publish(zip_file_queue, file))
+
+        # Then the consumer can read them
+        new_list: list[str] = []
+        while True:
+            file: str = asyncio.run(consume(zip_file_queue))
+            if file is None:
+                break
+            else:
+                new_list.append(file)
+
+        assert new_list == dirlist
